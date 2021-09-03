74faf31b6f1617c78fffce0d2900156d0141742b
上田靖丘 <white.5x20.stone@outlook.jp>
2021-09-03 21:41:51 +0900

    フォロー機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
-  
+
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
+
   root 'posts#index'
+  
 end
